1e1f12637b06cb250ff2275e367dff212f70d3c1
unknown <serenelamartinez@iresm.edu.ar>
2022-11-09 09:55:26 -0300

    Fondo cambiado de chocotorta

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import Button from "../../components/Button";
 import {useNavigate} from "react-router-dom"
-import background from "../../assets/maxresdefault.jpg";
+import background from "../../assets/chocotorta-destacada.jpg";
+
 
 // "a"
 function Home () {
@@ -12,7 +13,7 @@ function Home () {
     <section className="hero is-medium is-fullheight" style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>;
     <div className="hero-body">
         <div className="container has-text-centered">
-      <Button onClick={()=>onClickButton("/game")} text="Haz click para iniciar el juego"/>
+      <Button onClick={()=>onClickButton("/game")} text="Haz click para iniciar el juego DE LA CHOCOTORTA"/>
         </div>
     </div>
     </section>
